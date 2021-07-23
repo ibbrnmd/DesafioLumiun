67dcbdb7fb6315301b3e6c268cd4e316dcb7cad5
Bruno Delabari <brunomidon96@gmail.com>
2021-07-23 14:33:28 -0300

    initial commit

---
+++ database/migrations/2021_07_22_232836_create_desafios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDesafiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('desafios', function (Blueprint $table) {
+            $table->id();
+            $table->string();
+            $table->string('title');
+            $table->string('publisher');
+            $table->string('author');
+            $table->integer('pgNumber');
+            $table->string ('category');
+            $table->string('releasingYear');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('desafios');
+    }
+}
